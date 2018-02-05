b1ff775a97b5381850b7a0423f21c1f1273542ff
Melvin <melzzkwok@hotmail.com>
2018-02-05 14:55:18 +0800

    update reward.php

--- app/api/reward.php
+++ app/api/reward.php
@@ -78,36 +78,51 @@ $app->post('/api/reward/redeemreward', function(Request $request, Response $resp
     $result1 = $stmt1->fetchAll(PDO::FETCH_OBJ);
     $result2 = $stmt2->fetchAll(PDO::FETCH_OBJ);
     $result3 = $stmt3->fetchAll(PDO::FETCH_OBJ);
-    $count = $stmt3->rowCount();
-
-    $rewardpoint_total = $result1[0]->rewardpoint_total;
-    //echo $rewardpoint_total;
-    $reward_unlock_pts = $result2[0]->reward_unlock_pts;
-    //echo $reward_unlock_pts;
+    $count1 = $stmt1->rowCount();
+    $count2 = $stmt2->rowCount();
+    $count3 = $stmt3->rowCount();
     //echo $count;
-    if (($rewardpoint_total == null) || ($reward_unlock_pts == null)){
+
+    if (($count1 == null) || ($count2 == null)){
       echo '{"error":"no result"}';
     }
-    elseif ($count == null) {
-      if ($reward_unlock_pts <= $rewardpoint_total){
-        $sql = "INSERT INTO goal.user_reward(user_id, reward_id) VALUES ($user_id, $reward_id)";
+    else {
+      $rewardpoint_total = $result1[0]->rewardpoint_total;
+      //echo $rewardpoint_total;
+      $reward_unlock_pts = $result2[0]->reward_unlock_pts;
+      //echo $reward_unlock_pts;
 
-        $stmt3 = $db->prepare($sql);
-        $stmt3->execute();
-        echo '{"NOTICE":"reward redeem"}';
+      if ($count3 == null){
+        if ($reward_unlock_pts <= $rewardpoint_total){
+          $sql1 = "INSERT INTO goal.user_reward(user_id, reward_id) VALUES ($user_id, $reward_id)";
+          $stmt4 = $db->prepare($sql1);
+          $stmt4->execute();
+
+          $sql2 = "SELECT user_reward.userReward_id, goal_reward.reward_name, goal_reward.reward_description, goal_reward.reward_img FROM goal.user_reward JOIN goal.goal_reward WHERE user_reward.user_id = $user_id AND user_reward.reward_id = $reward_id AND user_reward.reward_id = goal_reward.reward_id";
+          $stmt5 = $db->query($sql2);
+          $result5 = $stmt5->fetchAll(PDO::FETCH_OBJ);
+          echo json_encode($result5);
+          echo '{"NOTICE":"reward redeem"}';
+        }
+
+        else {
+          echo '{"NOTICE":"not enough points to redeem"}';
+        }
+
       }
 
       else {
-        echo '{"NOTICE":"not enough points to redeem"}';
+        $sql3 = "SELECT user_reward.userReward_id, goal_reward.reward_name, goal_reward.reward_description, goal_reward.reward_img FROM goal.user_reward JOIN goal.goal_reward WHERE user_reward.user_id = $user_id AND user_reward.reward_id = $reward_id AND user_reward.reward_id = goal_reward.reward_id";
+        $stmt6 = $db->query($sql3);
+        $result6 = $stmt6->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($result6);
+        echo '{"NOTICE":"reward already redeemed"}';
       }
-    }
-    else {
-      echo '{"NOTICE":"reward already redeemed"}';
     }
   }
   catch(PDOException $e)
   {
-   echo '{"error":'.$e->getMessage().'}';
+    echo '{"error":'.$e->getMessage().'}';
 
   }
 
